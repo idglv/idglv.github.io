21460abeb4ac6620633e23f904f84655e6477034
Ivan Dyagilev <idglv@outlook.com>
2015-05-31 18:32:08 +0300

    add desktop verison

---
+++ app/desktopView/app.controller.js
@@ -0,0 +1,119 @@
+(function() {
+    "use strict";
+
+    sap.ui.controller("sap.app.view.app", {
+        onInit: function() {
+            var aColumnData = [{
+                columnId: "id",
+                columnText: "Идентификатор",
+                width: "20%"
+            }, {
+                columnId: "short",
+                columnText: "Наименование краткое",
+                width: "25%"
+            }, {
+                columnId: "name",
+                columnText: "Наименование полное",
+                width: "35%"
+            }, {
+                columnId: "tag",
+                columnText: "Ключевые слова",
+                width: "20%"
+            }];
+
+            var aData = [];
+            var id = 100;
+            while (id) {
+            	id--;
+            	aData.push(this.generateTable(id))
+            }
+
+            var aTree = [{
+            	name: "Единая модель объектов ТХГ(ЕМ ТХГ)",
+            	hasExpander: true,
+            	next: [{
+                	name: "Класификация объектов ТХГ",
+                	hasExpander: true,
+                	next: [{
+                		name: "Единая система газоснабжения",
+                		hasExpander: false,
+                		key: 0
+                	}, {
+                		name: "Линейная часть магистральных трубопроводов",
+                		hasExpander: false,
+                		key: 1
+                	}, {
+                		name: "Газораспределительная станция",
+                		hasExpander: false,
+                		key: 2
+                	}, {
+                		name: "Компрессорная станция",
+                		hasExpander: false,
+                		key: 3
+                	}, {
+                		name: "Объекты добычи газа",
+                		hasExpander: false,
+                		key: 4
+                	}, {
+                		name: "Объекты общего назначения",
+                		hasExpander: false,
+                		key: 5
+                	}, {
+                		name: "Объекты подземного хранения газа",
+                		hasExpander: false,
+                		key: 6
+                	}, {
+                		name: "Линейная часть магистральных трубопроводов",
+                		hasExpander: false,
+                		key: 7
+                	}]
+                }]
+            }]
+
+
+            var oModel = new sap.ui.model.json.JSONModel();
+
+            oModel.setData({
+                columns: aColumnData,
+                rows: aData,
+                tree: aTree
+            });
+
+            this.getView().setModel(oModel);
+
+            this.cache();
+        },
+
+        cache: function() {
+        	this.mainTable = this.getView().byId("mainTable");
+        },
+
+        handleSelectTree: function(e) {
+        	var key = e.getParameter("node").data("key");
+        	
+        	this.mainTable.bindAggregation("rows", {
+		    	path: "/rows",
+		    	sorter: [new sap.ui.model.Sorter("id", false)],
+		    	filters: [new sap.ui.model.Filter("key", "EQ", key)]
+		    });
+        },
+
+    	generateTable: function(id) {
+    		function rand(mArr) {
+    			return mArr[Math.random() * mArr.length << 0];
+    		}
+
+    		var shorts = ["Труба", "Кран", "Ключ", "Компрессор", "Двигатель", "Деталь", "Станция", "Защита"];
+    		var names = ["Кран шаровой ПА1022.2503.63", "Труба стальная ГОСТ 10704-91", "Цех №1 комрессорный"];
+    		var key = [0, 1, 2, 3, 4, 5, 6];
+    		return {
+    			id: id,
+    			key: rand(key),
+                short: rand(shorts),
+                name: rand(names),
+                tag: rand(shorts)
+    		}
+    	}
+    });
+
+})()
---
+++ app/desktopView/app.view.js
@@ -0,0 +1,99 @@
+(function(){
+	"use strict";
+
+	sap.ui.jsview("sap.app.view.app", {
+
+		/** Specifies the Controller belonging to this View. 
+		* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+		* @memberOf trea.app
+		*/ 
+		getControllerName : function() {
+			return "sap.app.view.app";
+		},
+
+		/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+		* Since the Controller is given to this method, its event handlers can be attached right away. 
+		* @memberOf trea.app
+		*/ 
+		createContent : function(oController) {
+			//tree
+			var oTree = new sap.ui.commons.Tree({
+				title: "ОАО \"Газпром\"",
+				height: "315px",
+				layoutData: new sap.ui.layout.GridData({
+					span: "L4 M4 S4"
+				}),
+				select: [oController.handleSelectTree, oController]
+			});
+
+			var oFactSheetNodeTemplate = new sap.ui.commons.TreeNode({
+				text: "{name}",
+				expanded: false,
+				hasExpander: "{hasExpander}",
+				selectable: {
+					path: "hasExpander",
+					formatter: function(bSelectable) {
+						return !bSelectable;
+					} 
+				},
+				customData: [{"key": "key", "value": "{key}"}]
+			});
+		
+			oTree.bindAggregation("nodes", {
+				path: "/tree",
+				template: oFactSheetNodeTemplate,
+				parameters:{
+					navigation : {
+						'next' : 'next'
+					}
+				}
+			});
+
+			//table
+			var oTable = new sap.ui.table.Table(this.createId("mainTable"), {
+				width: "100%",
+				layoutData: new sap.ui.layout.GridData({
+					span: "L8 M8 S8"
+				}),
+				navigationMode: sap.ui.table.NavigationMode.Paginator,
+				selectionMode: sap.ui.table.SelectionMode.None
+			});
+
+			oTable.bindColumns("/columns", function(index, context) {
+		        var sColumnId = context.getObject().columnId;
+		        var sColumnText = context.getObject().columnText;
+		        var width = context.getObject().width;
+
+		        return new sap.ui.table.Column({
+		            label: sColumnText,
+		            template: sColumnId,
+		            sortProperty: sColumnId,
+		            width: width
+		        });
+		    });
+
+			//panel
+			var oPanel = new sap.ui.commons.Panel({
+				title: new sap.ui.core.Title({
+					//text: "Классификаторы",
+					icon: "img/logo.png"
+				}),
+				showCollapseIcon: false,
+				layoutData: new sap.ui.layout.GridData({
+					span: "L12 M12 S12"
+				}),
+			}).addStyleClass("logo");
+			
+		    //grid
+		    var oGrid = new sap.ui.layout.Grid({
+		    	hSpacing: 1,
+				vSpacing: 1,
+		    	content: [oPanel, oTree, oTable]
+		    });
+
+			return oGrid;
+		}
+	});
+
+})();
+
