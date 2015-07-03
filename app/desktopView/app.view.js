e60e25966484858298d4c02d5071f47a6fd74d95
idglv <idglv@outlook.com>
2015-07-03 17:45:21 +0300

    minor changes

--- app/desktopView/app.view.js
+++ app/desktopView/app.view.js
@@ -3,18 +3,10 @@
 
 	sap.ui.jsview("sap.app.desktopView.app", {
 
-		/** Specifies the Controller belonging to this View. 
-		* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-		* @memberOf trea.app
-		*/ 
 		getControllerName : function() {
 			return "sap.app.desktopView.app";
 		},
 
-		/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
-		* Since the Controller is given to this method, its event handlers can be attached right away. 
-		* @memberOf trea.app
-		*/ 
 		createContent : function(oController) {
 			//tree
 			var oTree = new sap.ui.commons.Tree({
--- app/util/rawToTree.js
+++ app/util/rawToTree.js
@@ -1,29 +1,29 @@
 sap.ui.define([],
-    function() {
+	function() {
 
-        return function(fn) {
-            jQuery.getJSON("./data/raw.json", function(raw) {
-            	var data = Object.create(null);
-            	var result = [];
+	return function(fn) {
+		jQuery.getJSON("./data/raw.json", function(raw) {
+			var data = Object.create(null);
+			var result = [];
 
-            	raw.forEach(function(line) {
-            		data[line.id] = line;
-            	});
+			raw.forEach(function(line) {
+				data[line.id] = line;
+			});
 
-            	raw.forEach(function(line) {
-            		if (line.parentId === 0) {
-            			result.push(data[line.id]);
-            		} else {
-            			data[line.parentId].next ? 
-            			data[line.parentId].next.push(data[line.id]) :
-            			data[line.parentId].next = [data[line.id]];
+			raw.forEach(function(line) {
+				if (line.parentId === 0) {
+					result.push(data[line.id]);
+				} else {
+					data[line.parentId].next ?
+						data[line.parentId].next.push(data[line.id]) :
+						data[line.parentId].next = [data[line.id]];
 
-            			data[line.parentId].hasExpander = true;
-            		}
-            	});
+					data[line.parentId].hasExpander = true;
+				}
+			});
 
-            	fn.call(null, result);
-            });
-        }
+			fn.call(null, result);
+		});
+	}
 
-    }, true)
+}, true)
