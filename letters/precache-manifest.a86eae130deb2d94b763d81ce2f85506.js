45f816ba55525cc27d6d2548534f9708289afaf2
idglv <idglv@outlook.com>
2019-03-17 23:06:21 +0300

    Letters new version demo

---
+++ letters/precache-manifest.a86eae130deb2d94b763d81ce2f85506.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fb01e7021e891bc73a52",
+    "url": "/static/js/main.fb01e702.chunk.js"
+  },
+  {
+    "revision": "a3e903d1e564f20cf8c6",
+    "url": "/static/js/1.a3e903d1.chunk.js"
+  },
+  {
+    "revision": "fb01e7021e891bc73a52",
+    "url": "/static/css/main.0b990a82.chunk.css"
+  },
+  {
+    "revision": "52f0309fd48a18db1c9533cb1dd41b73",
+    "url": "/index.html"
+  }
+];
